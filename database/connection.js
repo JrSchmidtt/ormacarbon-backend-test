7a276a72be460f883706b046918dae8d57ca787c
Junior Schmidt <junior.schmidt.99@gmail.com>
2022-06-22 20:00:31 -0300

    Setup Files

---
+++ database/connection.js
@@ -0,0 +1,10 @@
+const Sequelize = require('sequelize');
+const connection = new Sequelize('carbon_competition','root','',{
+    host:'localhost',
+    dialect:'mysql',
+    timezone: '-03:00'
+});
+
+module.exports = connection;
+
+
--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-{
-  "name": "blog-express",
-  "version": "1.0.0",
-  "description": "",
-  "main": "index.js",
-  "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1"
-  },
-  "author": "",
-  "license": "ISC",
-  "dependencies": {
-    "body-parser": "^1.20.0",
-    "ejs": "^3.1.8",
-    "express": "^4.18.1",
-    "mysql2": "^2.3.3",
-    "nodemon": "^2.0.16",
-    "sequelize": "^6.19.0"
-  }
-}
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const database = require('./database/connection');
+const port = 3000
+
+// View Engine
+app.set('view engine', 'ejs');
+
+// Static
+app.use(express.static('public'));
+
+// Body Parser
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) =>{
+    res.render('index');
+})
+
+// database MYSQL
+database
+    .authenticate()
+    .then(() => {
+        console.log('Database connected!')
+    }).catch((error) =>{
+        console.log(error);
+    })
+
+app.listen(port, () => console.log(`API listening on port ${port}!`))
